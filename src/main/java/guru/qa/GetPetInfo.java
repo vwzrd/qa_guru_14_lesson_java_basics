552ac9a269900cf707fef9f530db49db25ade75b
Maxim_Gavrikov <destrallhumn@gmail.com>
2022-08-04 12:26:02 +0300

    added new method .getRelativeAge()

--- src/main/java/guru/qa/GetPetInfo.java
+++ src/main/java/guru/qa/GetPetInfo.java
@@ -2,7 +2,8 @@ package guru.qa;
 
 public class GetPetInfo {
     public static void main(String[] args) {
-        Pet gena = new Pet("Геннадий", 2, true);
+        Pet gena = new Pet("Геннадий", 0, 0.5, true);
+        gena.getRelativeAge();
         gena.animalSpeak();
     }
 }
--- src/main/java/guru/qa/Pet.java
+++ src/main/java/guru/qa/Pet.java
@@ -3,17 +3,21 @@ package guru.qa;
 public class Pet {
     private String name;
     private int legs;
+    private double age;
     private boolean isAquatic;
     private String animal;
+    private double lifespan;
+    private double percentage;
 
-    public Pet(String name, int legs, boolean isAquatic) {
+    public Pet(String name, int legs, double age, boolean isAquatic) {
         this.name = name;
         this.legs = legs;
+        this.age = age;
         this.isAquatic = isAquatic;
     }
 
     private void sayTheAnimal() {
-            System.out.println(name + " - это " + animal + "!");
+        System.out.println(name + " - это " + animal + "!");
     }
 
     void guessTheAnimal() {
@@ -21,22 +25,27 @@ public class Pet {
             case 0:
                 if (isAquatic) {
                     this.animal = "Рыба";
+                    this.lifespan = 3;
                 } else {
                     this.animal = "Змея";
+                    this.lifespan = 15;
                 }
                 break;
             case 2:
                 if (isAquatic) {
                     this.animal = "Неизвестный зверь";
                 } else {
-                this.animal = "Попугай";
+                    this.animal = "Попугай";
+                    this.lifespan = 20;
                 }
                 break;
             case 4:
                 if (isAquatic) {
                     this.animal = "Черепаха";
+                    this.lifespan = 30;
                 } else {
                     this.animal = "Кошка";
+                    this.lifespan = 15;
                 }
                 break;
             default:
@@ -70,5 +79,33 @@ public class Pet {
                 System.out.println("Неизвестный зверь отказывается говорить.");
         }
     }
+
+    void getRelativeAge() {
+        if (animal == null) {
+            System.out.println("Сначала узнаем что это за животное.");
+            guessTheAnimal();
+        }
+        if (age <= 0) {
+            if (age == 0) {
+                System.out.println(name + " новорожденный.");
+            } else if (age < 0) {
+                System.out.println(name + " еще не родился.");
+            }
+        } else if (animal == "Неизвестный зверь") {
+            System.out.println("Неизвестно сколько живут неизвестные звери.");
+        } else {
+            percentage = age / lifespan * 100;
+            if (percentage > 0 && percentage <= 33) {
+                System.out.println(name + " еще детёныш.");
+            } else if (percentage > 33 && percentage <= 66) {
+                System.out.println(name + " - взрослый зверь.");
+            } else if (percentage > 66 && percentage <= 99) {
+                System.out.println(name + " уже старичок.");
+            } else {
+                System.out.println(name + " - долгожитель!");
+            }
+        }
+    }
 }
+
 
